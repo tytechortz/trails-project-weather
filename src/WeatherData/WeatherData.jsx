fbc48cb79090e04127f14976b449b076c9c1a894
James <tytechortz@gmail.com>
2018-11-03 14:23:13 -0600

    sets up weather forecast files

---
+++ src/WeatherData/WeatherData.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const weatherData = (props) => {
+
+    return (
+        <div>
+            <h1>5 Day Forecast</h1>
+        </div>
+    )
+}
+
+export default weatherData;
