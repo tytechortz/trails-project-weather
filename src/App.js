b64436170cd668606f25effd25fd6d5a3b75c799
James <tytechortz@gmail.com>
2018-11-03 14:35:13 -0600

    adds app.js

--- src/App.js
+++ src/App.js
@@ -11,13 +11,30 @@ class App extends Component {
     }
   }
 
+getWeather = async () => {
 
+  try{
+    const weather = await fetch('http://api.openweathermap.org/data/2.5/forecast?zip=80206,us&APPID=d177ecaf5436da5c14f410fb73b64b4d');
+    const weatherJson = await weather.json();
+    return weatherJson
+  } catch(err){
+    return err
+  }
+}
 
+componentDidMount(){
+  this.getWeather().then((data) => {
+    console.log(data, 'this is data');
+    this.setState({weather: data});
 
+  }).catch((err) => {
+    console.log(err);
+  });
+}
   render() {
     return (
       <div className="App">
-      
+        <WeatherData weather={this.state.weather} />
       </div>
     );
   }
