3ce209ab95036bd1d12e3caeafc255d193aae1f9
James <tytechortz@gmail.com>
2018-11-04 10:38:55 -0700

    changes weather to lat/lon location and imperial units

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import WeatherData from './WeatherData/WeatherData';
-import './App.css';
+
+
+
+const lat = '39';
+const lon = '-104';
+const API_Key = 'd177ecaf5436da5c14f410fb73b64b4d'
 
 class App extends Component {
   constructor(){
@@ -14,7 +19,7 @@ class App extends Component {
 getWeather = async () => {
 
   try{
-    const weather = await fetch('http://api.openweathermap.org/data/2.5/forecast?zip=80206,us&APPID=d177ecaf5436da5c14f410fb73b64b4d');
+    const weather = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&APPID=${API_Key}`);
     const weatherJson = await weather.json();
     return weatherJson
   } catch(err){
--- src/WeatherData/WeatherData.jsx
+++ src/WeatherData/WeatherData.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+
+
+
 const weatherData = (props) => {
+    console.log(props)
+    
 
     return (
         <div>
-            <h1>5 Day Forecast</h1>
+            <h3>Current Weather</h3>
+            <ul>
+                <p>Temp: {props.weather && props.weather.main && props.weather.main.temp}</p>
+                <p>Humidity: {props.weather && props.weather.main && props.weather.main.humidity}</p>
+                <p>High: {props.weather && props.weather.main && props.weather.main.temp_max}</p>
+                <p>Low: {props.weather && props.weather.main && props.weather.main.temp_min}</p>
+            </ul>
         </div>
     )
 }
